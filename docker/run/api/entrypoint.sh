65a0a1f668c7ab9cae56124fed0bdc847828e371
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-11-04 21:41:24 +0700

    Add worker's entrypoint

--- docker/run/api/entrypoint.sh
+++ docker/run/api/entrypoint.sh
@@ -2,7 +2,7 @@
 
 dockerize -wait tcp://db:3306
 
-php bin/console doctrine:database:create
+php bin/console doctrine:database:create  --if-not-exists
 php bin/console doctrine:schema:update --force
 
 php bin/console server:run *:80
