1ebf9d3f83ccc8861e4e3c28364019d674c730ea
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-11-04 23:07:08 +0700

    Helper to build docker images faster

---
+++ docker/build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+docker rmi tienvx/mbt
+docker rmi tienvx/mbt-worker
+docker rmi tienvx/mbt-e2e-worker
+docker rmi tienvx/mbt-api
+
+docker build -t tienvx/mbt ./mbt
+docker build -t tienvx/mbt-worker ./mbt-worker
+docker build -t tienvx/mbt-e2e-worker ./mbt-e2e-worker
+docker build -t tienvx/mbt-api ./mbt-api
+
+docker push tienvx/mbt
+docker push tienvx/mbt-worker
+docker push tienvx/mbt-e2e-worker
+docker push tienvx/mbt-api
