84e479c926c0b2aa6d5dc50b87dce884d5b58be2
tien.xuan.vo <tien.xuan.vo@gmail.com>
2018-12-15 16:09:21 +0700

    Move code to mbt-infrastructure

--- docker/run/api/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-dockerize -wait tcp://db:3306
-
-php bin/console doctrine:database:create  --if-not-exists
-php bin/console doctrine:schema:update --force
-
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
